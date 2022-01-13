d2a5deba18cfd5511f21ae6823ac4081d4fb7f32
Delon Newman <contact@delonnewman.name>
2022-01-13 14:03:43 -0700

    Minibars::Template#call - Encode parameters with JSON.generate

--- lib/minibars.rb
+++ lib/minibars.rb
@@ -4,11 +4,14 @@ require 'mini_racer'
 require 'json'
 
 module Minibars
+  class Error < RuntimeError; end
+  EMPTY_HASH = {}.freeze
+
   class Context
     HANDLE_BARS_FILE = Pathname.new(__dir__).join('../vendor/javascript/handlebars.js')
 
     attr_reader :js
-    
+
     def initialize(handlebars_file: nil)
       @js = MiniRacer::Context.new.tap do |js|
         js.load(handlebars_file || HANDLE_BARS_FILE)
@@ -20,6 +23,9 @@ module Minibars
     end
 
     def register_partial(name, content)
+      raise Error, 'Partial name should be a string'    unless name.is_a?(String)
+      raise Error, 'Partial content should be a string' unless content.is_a?(String)
+
       @js.eval("Handlebars.registerPartial(#{name.to_json}, #{content.to_json})")
 
       self
@@ -46,7 +52,7 @@ module Minibars
     def self.compile(context, content)
       new(context, content).compile
     end
-    
+
     def initialize(context, content)
       @content = content
       @context = context
@@ -54,13 +60,15 @@ module Minibars
     end
 
     def compile
+      raise Error, 'Template content should be a string' unless content.is_a?(String)
+
       @context.js.eval("#{name} = Handlebars.compile(#{content.to_json})")
 
       self
     end
 
-    def call(params = {})
-      @context.js.eval("#{name}(#{params.to_json})")
+    def call(params = EMPTY_HASH)
+      @context.js.eval("#{name}(#{JSON.generate params})")
     end
 
     private
